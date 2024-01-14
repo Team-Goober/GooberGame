6c66e808cb53e8840b5122049149cadde6c3dac7
joyan <joyan@YANG-PC>
2024-01-14 14:30:32 -0500

    Add project files.

---
+++ Sprint/Controllers/KeyboardC.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Xna.Framework.Input;
+using Sprint.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sprint.Controllers
+{
+    /*
+     * Invoker
+     */
+    public class KeyboardC : IController
+    {
+        private KeyboardState oldState;
+        private Dictionary<Keys, ICommand> keyActions;
+
+        public KeyboardC(KeyboardState newState) 
+        { 
+            this.oldState = newState;
+
+            keyActions = new Dictionary<Keys, ICommand>();
+        }
+
+        public void UpdateInput()
+        {
+            
+        }
+    }
+}
---
+++ Sprint/Program.cs
@@ -0,0 +1,3 @@
+﻿
+using var game = new Sprint.Game1();
+game.Run();
