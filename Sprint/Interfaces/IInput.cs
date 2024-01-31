16f2fc888e7fe102ff2b91066c851dc51b562837
Pqvqn <pavanarauch@hotmail.com>
2024-01-31 18:18:43 -0500

    Refactor input interfaces

---
+++ Sprint/Interfaces/IInput.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework;
+
+namespace Sprint.Interfaces
+{
+    internal interface IInput
+    {
+        
+        // Update internal vars based on controllers
+        void UpdateInput(GameTime gameTime, KeyboardState keys, MouseState mouse);
+
+        // Return whether all requirements for this input trigger are met
+        bool IsSatisfied();
+
+
+    }
+}
---
+++ Sprint/Interfaces/IInputMap.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Sprint.Interfaces
+{
+    internal interface IInputMap
+    {
+        // Register input trigger to execution of command
+        void RegisterMapping(IInput input, ICommand command);
+
+        // Find active inputs and run corresponding commands each cycle
+        void Update(GameTime gameTime);
+
+    }
+}
