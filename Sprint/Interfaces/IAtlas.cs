6c494f2f52da9743ca8f5d75a454bd61c37d86eb
Pqvqn <pavanarauch@hotmail.com>
2024-01-31 19:30:02 -0500

    ISprite and IAtlas

---
+++ Sprint/Interfaces/IAtlas.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Sprint.Interfaces
+{
+    public interface IAtlas
+    {
+        // Enable or disable animation looping
+        void SetLooping(bool loop);
+
+        // Set number of frames displayed per second
+        void SetFramerate(int fps);
+
+        // Get bounds of current frame on spritesheet
+        Rectangle CurrentFrame();
+
+        // Check whether frame should be changed each update cycle
+        void Update(GameTime gameTime);
+
+        // Return animation to first frame
+        void Reset();
+
+    }
+}
