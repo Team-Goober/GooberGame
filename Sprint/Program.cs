ccab60e57feda03fb1c06ad29840b1456237a43e
Pqvqn <pavanarauch@hotmail.com>
2024-02-05 13:33:08 -0500

    Ability to run another game object using test command line argument

--- Sprint/Program.cs
+++ Sprint/Program.cs
@@ -1,3 +1,19 @@
 ﻿
-using var game = new Sprint.Game1();
+using Microsoft.Xna.Framework;
+using System;
+
+string[] c_args = Environment.GetCommandLineArgs();
+
+Game game;
+
+if (c_args.Length == 2 && c_args[1].Equals("test"))
+{
+    game = new Sprint.Testing.GameTest();
+}
+else
+{
+    game = new Sprint.Game1();
+}
+
+
 game.Run();
