ead59ea299427f07e3819071567086c63e589420
Pqvqn <pavanarauch@hotmail.com>
2024-02-05 11:09:15 -0500

    Parented Auto and ManualAtlas as TimedAtlas

--- Sprint/Sprite/AutoAtlas.cs
+++ Sprint/Sprite/AutoAtlas.cs
@@ -4,71 +4,34 @@ using System;
 
 namespace Sprint.Sprite
 {
-    internal class AutoAtlas : IAtlas
+    internal class AutoAtlas : TimedAtlas, IAtlas
     {
         private Rectangle sheetArea;
         private int rows;
         private int cols;
         private int padding;
 
-        private int frame;
-        private int frameCount;
-
-        private bool looping;
-        private float fps;
-        private TimeSpan timeAtLastFrame;
-
-        public AutoAtlas(Rectangle sheetArea, int rows, int cols, int padding, bool loop, float framerate)
+        public AutoAtlas(Rectangle sheetArea, int rows, int cols, int padding, bool loop, float framerate): base(rows*cols, loop, framerate)
         {
             this.sheetArea = sheetArea;
             this.rows = rows;
             this.cols = cols;
             this.padding = padding;
-
-            frame = 0;
-            frameCount = rows * cols;
-
-            looping = loop;
-            fps = framerate;
         }
 
-        // Move to next frame
-        private void advance()
-        {
-            // Increment once linearly through frames
-            if (frame < frameCount - 1)
-            {
-                frame++;
-            }
-            // Loop around if looping enabled
-            else if (looping)
-            {
-                frame = 0;
-            }
-        }
-
-        public void SetLooping(bool loop)
-        {
-            looping = loop;
-        }
-
-        public void SetFramerate(float fps)
-        {
-            this.fps = fps;
-        }
-
-        public Vector2 CurrentCenterPoint()
+        override public Vector2 CurrentCenterPoint()
         {
             // Centerpoint is top-left corner
             return Vector2.Zero;
         }
-        public float CurrentDuration()
+
+        override public float CurrentDuration()
         {
             // Frames have constant timing
             return 1.0f;
         }
 
-        public Rectangle CurrentFrame()
+        override public Rectangle CurrentFrame()
         {
             // Calculate the row and column indices of the current frame
             int paddedWidth = (sheetArea.Width + padding) / cols;
@@ -79,22 +42,6 @@ namespace Sprint.Sprite
             // Calculate bounds of the frame as Rectangle
             Rectangle sourceRectangle = new Rectangle(sheetArea.X + paddedWidth * column, sheetArea.Y + paddedHeight * row, paddedWidth - padding, paddedHeight - padding);
             return sourceRectangle;
-        }
-
-        public void Update(GameTime gameTime)
-        {
-            // Switch frames if needed and based on framerate
-            if (CurrentDuration() > 0 && fps > 0
-               && (gameTime.TotalGameTime - timeAtLastFrame).TotalSeconds > CurrentDuration() / fps)
-            {
-                advance();
-                timeAtLastFrame = gameTime.TotalGameTime;
-            }
-        }
-
-        public void Reset()
-        {
-            frame = 0;
         }
     }
 }
--- Sprint/Sprite/ManualAtlas.cs
+++ Sprint/Sprite/ManualAtlas.cs
@@ -4,81 +4,34 @@ using System;
 
 namespace Sprint.Sprite
 {
-    internal class ManualAtlas : IAtlas
+    internal class ManualAtlas : TimedAtlas, IAtlas
     {
 
         private Rectangle[] rects;
         private Vector2[] centers;
         private float[] durs;
-        private int frame;
 
-        private bool looping;
-        private float fps;
-        private TimeSpan timeAtLastFrame;
-
-        public ManualAtlas(Rectangle[] rectangles, Vector2[] centerPoints, float[] durations)
+        public ManualAtlas(Rectangle[] rectangles, Vector2[] centerPoints, float[] durations, bool loop, float framerate) : base(rectangles.Length, loop, framerate)
         {
             this.rects = rectangles;
             this.centers = centerPoints;
             this.durs = durations;
-            frame = 0;
         }
 
 
-        // Move to next frame
-        public void advance()
-        {
-            // Increment once linearly through frames
-            if (frame < rects.Length - 1)
-            {
-                frame++;
-            }
-            // Loop around if looping enabled
-            else if (looping)
-            {
-                frame = 0;
-            }
-        }
-
-        public Vector2 CurrentCenterPoint()
+        override public Vector2 CurrentCenterPoint()
         {
             return centers[frame];
         }
 
-        public float CurrentDuration()
+        override public float CurrentDuration()
         {
             return durs[frame];
         }
 
-        public Rectangle CurrentFrame()
+        override public Rectangle CurrentFrame()
         {
             return rects[frame];
-        }
-
-        public void Reset()
-        {
-            frame = 0;
-        }
-
-        public void SetLooping(bool loop)
-        {
-            looping = loop;
-        }
-
-        public void SetFramerate(float fps)
-        {
-            this.fps = fps;
-        }
-
-        public void Update(GameTime gameTime)
-        {
-            // Switch frames if needed and based on framerate
-            if (CurrentDuration() > 0 && fps > 0
-               && (gameTime.TotalGameTime - timeAtLastFrame).TotalSeconds > CurrentDuration() / fps)
-            {
-                advance();
-                timeAtLastFrame = gameTime.TotalGameTime;
-            }
         }
 
     }
