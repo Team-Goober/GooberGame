21937b3f35487c19aa1e3c7bccf0099ccb31284f
Pqvqn <pavanarauch@hotmail.com>
2024-02-05 00:52:24 -0500

    SingleAtlas implementation for single frames

--- Sprint/Input/SingleKeyPressTrigger.cs
+++ Sprint/Input/SingleKeyPressTrigger.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
 using Sprint.Interfaces;
+
 namespace Sprint.Input
 {
     internal class SingleKeyPressTrigger : IInputTrigger
