b4905b861f81bf711cc94a3f6e970842c69d283b
Bill Yang <yang.6152@buckeyemail.osu.edu>
2024-02-05 18:48:33 -0500

    Add item cycling

---
+++ Sprint/CycleItem.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+using Sprint.Interfaces;
+using Sprint.Sprite;
+using System;
+
+
+
+
+namespace Sprint
+{
+    internal class CycleItem
+    {
+        private List<ISprite> sprites = new List<ISprite>();
+        private int position;
+
+        public CycleItem(Game1 game) 
+        {
+            position = 0;
+
+            Texture2D rupyT = game.Content.Load<Texture2D>("Items/ZeldaSprite5Rupies");
+            ISprite rupyS = new AnimatedSprite(rupyT);
+
+            IAtlas rupyA = new SingleAtlas(new Rectangle(0, 0, 8, 16), new Vector2(4, 8));
+            rupyS.RegisterAnimation("rupy", rupyA);
+
+            rupyS.SetAnimation("rupy");
+            rupyS.SetScale(4);
+            sprites.Add(rupyS);
+
+            Texture2D arrowT = game.Content.Load<Texture2D>("Items/ZeldaSpriteArrow");
+            ISprite arrowS = new AnimatedSprite(arrowT);
+
+            IAtlas arrowA = new SingleAtlas(new Rectangle(0, 0, 5, 16), new Vector2(2, 8));
+            arrowS.RegisterAnimation("arrow", arrowA);
+
+            arrowS.SetAnimation("arrow");
+            arrowS.SetScale(4);
+            sprites.Add(arrowS);
+
+            Texture2D heartConT = game.Content.Load<Texture2D>("Items/ZeldaSpriteHeartContainer");
+            ISprite heartConS = new AnimatedSprite(heartConT);
+
+            IAtlas heartConA = new SingleAtlas(new Rectangle(0, 0, 13, 13), new Vector2(5, 5));
+            heartConS.RegisterAnimation("heartCon", heartConA);
+
+            heartConS.SetAnimation("heartCon");
+            heartConS.SetScale(4);
+            sprites.Add(heartConS);
+
+            Texture2D magicalShieldT = game.Content.Load<Texture2D>("Items/ZeldaSpriteMagicalShield");
+            ISprite magicalShieldS = new AnimatedSprite(magicalShieldT);
+
+            IAtlas magicalShieldA = new SingleAtlas(new Rectangle(0, 0, 8, 12), new Vector2(4, 6));
+            magicalShieldS.RegisterAnimation("magicalShield", magicalShieldA);
+
+            magicalShieldS.SetAnimation("magicalShield");
+            magicalShieldS.SetScale(4);
+            sprites.Add(magicalShieldS);
+
+            Texture2D triforceT = game.Content.Load<Texture2D>("Items/Triforce");
+            ISprite triforceS = new AnimatedSprite(triforceT);
+
+            IAtlas triforceA = new AutoAtlas(new Rectangle(0, 0, 24, 10), 1, 2, 4, true, 10);
+            triforceS.RegisterAnimation("triforce", triforceA);
+
+            triforceS.SetAnimation("triforce");
+            triforceS.SetScale(4);
+            sprites.Add(triforceS);
+        }
+
+        public void Next()
+        {
+            position++;
+
+            if(position == sprites.Count)
+            {
+                position = 0;
+            }
+        }
+
+        public void Back()
+        {
+            position--;
+            if(position == -1)
+            {
+                position = sprites.Count - 1;
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch, GameTime gameTime)
+        {
+            sprites[position].Draw(spriteBatch, new Vector2(500, 100), gameTime);
+        }
+    }
+}
--- Sprint/Game1.cs
+++ Sprint/Game1.cs
@@ -7,6 +7,7 @@ using Sprint.Interfaces;
 using System.Collections;
 using Sprint.Sprite;
 using System.Diagnostics;
+using Sprint.Commands;
 
 namespace Sprint
 {
@@ -14,11 +15,10 @@ namespace Sprint
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
-        //////////////////////////////////////////
-        private Lugi lugi;
-        private string animation;
-        //////////////////////////////////////////
+
         private IInputMap inputTable;
+
+        private CycleItem items;
 
         private SpriteFont font;
 
@@ -32,7 +32,6 @@ namespace Sprint
         protected override void Initialize()
         {
 
-            animation = "frozen";
 
             inputTable = new InputTable();
 
@@ -43,11 +42,10 @@ namespace Sprint
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            //////////////////////////////////////////
-            Texture2D texture = Content.Load<Texture2D>("lugi_left2");
-            Texture2D textureRight = Content.Load<Texture2D>("lugi_right");
-            lugi = new Lugi(texture, textureRight, 1, 4);
-            //////////////////////////////////////////
+            items = new CycleItem(this);
+
+            inputTable.RegisterMapping(new SingleKeyPressTrigger(Keys.I), new NextItem(items));
+            inputTable.RegisterMapping(new SingleKeyPressTrigger(Keys.U), new BackItem(items));
 
             font = Content.Load<SpriteFont>("Font");
         }
@@ -55,19 +53,17 @@ namespace Sprint
         protected override void Update(GameTime gameTime)
         {
 
-            //////////////////////////////////////////
-            lugi.Update(gameTime);
-            //////////////////////////////////////////
+
             inputTable.Update(gameTime);
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Aquamarine);
 
             _spriteBatch.Begin();
-            lugi.Draw(_spriteBatch, new Vector2(300, 200), animation);
+            items.Draw(_spriteBatch, gameTime);
             _spriteBatch.DrawString(font, "Credit", new Vector2(10, 300), Color.Black);
             _spriteBatch.DrawString(font, "Program Made By: Bill Yang", new Vector2(10, 330), Color.Black);
             _spriteBatch.DrawString(font, "Sprites from: www.mariomayhem.com/downloads/sprites/super_mario_bros_sprites.php", new Vector2(10, 360), Color.Black);
@@ -76,9 +72,5 @@ namespace Sprint
             base.Draw(gameTime);
         }
 
-        public void SetAnimation(string newAnimation)
-        {
-            this.animation = newAnimation;
-        }
     }
 }
