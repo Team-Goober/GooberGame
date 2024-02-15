b982a5c7de80f7140f44d5f0bf362bf085d17e49
haynes418 <lukehyns@gmail.com>
2024-02-15 18:10:30 -0500

    Comment about what is changed

---
+++ Sprint/test.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sprint
+{
+    internal class test
+    {
+        void methodone()
+        {
+            Console.WriteLine("test");
+        }
+        
+        void Method2()
+        {
+            Console.WriteLine("Hi");
+        }
+
+    }
+}
