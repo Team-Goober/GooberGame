1f95f694a14e4f0de564dc86198aa0c67e153409
Pqvqn <pavanarauch@hotmail.com>
2024-03-07 00:47:16 -0500

    Rename Level folder to Levels to avoid namespace conflict

--- Sprint/Loader/LevelLoader.cs
+++ Sprint/Loader/LevelLoader.cs
@@ -1,10 +1,5 @@
-﻿using Microsoft.Xna.Framework;
+﻿using Sprint.Levels;
 using Microsoft.Xna.Framework.Content;
-using Sprint.Characters;
-using Sprint.Interfaces;
-using Sprint.Level;
-using Sprint.Sprite;
-using System.Collections.Generic;
 using XMLData;
 
 namespace Sprint.Loader
@@ -24,7 +19,7 @@ namespace Sprint.Loader
         * 
         * @param path      Path to the XML file
         */
-        public void LoadXML(string path)
+        public Level LoadXML(string path)
         {
             Data = content.Load<LevelData>(path);
         }
