ac2f762d8825ddd9cdf0aa04e3e520a05a9b903e
MinsooKim142 <khjun7830@gmail.com>
2024-03-06 22:06:37 -0500

    have to test enemy factory

--- Sprint/Characters/EnemyFactory.cs
+++ Sprint/Characters/EnemyFactory.cs
@@ -6,19 +6,58 @@ namespace Sprint.Characters
     internal class EnemyFactory
     {
 
+
+        private Vector2 position;
+        private GameObjectManager objectManager;
+
+        public EnemyFactory(GameObjectManager objectManager, SpriteLoader spriteLoader)
+        {
+            this.position = pos;
+            this.objectManager = objectManager;
+        }
+
+
         /// <summary>
         /// Builds enemy from string name
         /// </summary>
         /// <param name="name">Name of enemy to make</param>
         /// <param name="position">World position to spawn at</param>
         /// <returns></returns>
-        public Character MakeEnemy(String name, Vector2 position)
+        public Enemy MakeEnemy(Goober game, String name, Vector2 position)
         {
             // TODO: Implement this function
             // Consider storing enemies in file with reflection, and having enemies load their own sprites
             // This would make dealing with enemies much easier
-            return null;
+            this.position = pos;
+            this.objectManager = objectManager;
+
+
+            ISprite enemySprite = spriteLoader.BuildSprite(ANIM_FILE, makeStr);
+
+            switch (name)
+            {
+                case "jellyfish":
+                    return new JellyfishEnemy(game, enemySprite, position, objectManager, spriteLoader);
+                case "bluebubble":
+                    return new BluebubbleEnemy(game, enemySprite, position, objectManager, spriteLoader);
+                case "skeleton":
+                    return new SkeletonEnemy(game, enemySprite, position, objectManager, spriteLoader);
+                case "dog":
+                    return new DogEnemy(game,enemySprite, position, objectManager, spriteLoader);
+                case "bat":
+                    return new BatEnemy(game, enemySprite, position, objectManager, spriteLoader);
+                case "hand":
+                    return new HandEnemy(game, enemySprite, position, objectManager, spriteLoader);
+                case "dragonmov":
+                    return new DragonEnemy(game, enemySprite, position, objectManager, spriteLoader);
+            }
+
+
+
         }
+
+
+
 
     }
 }
