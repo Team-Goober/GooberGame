7f9e1d7ef6b54b1666059054a417eed537651867
Pqvqn <pavanarauch@hotmail.com>
2024-03-07 19:25:05 -0500

    Added Gap tile

--- Sprint/Collision/CollisionHandler.cs
+++ Sprint/Collision/CollisionHandler.cs
@@ -32,6 +32,7 @@ namespace Sprint.Collision
         Dictionary<TypePairKey, ConstructorInfo> commandDictionary = new Dictionary<TypePairKey, ConstructorInfo>()
             {
                 {new TypePairKey(typeof(Player), typeof(WallTile)), typeof(PushMoverOut).GetConstructor(new Type[] {typeof(IMovingCollidable), typeof(Vector2)})},
+                {new TypePairKey(typeof(Player), typeof(GapTile)), typeof(PushMoverOut).GetConstructor(new Type[] {typeof(IMovingCollidable), typeof(Vector2)})},
                 {new TypePairKey(typeof(Player), typeof(InvisibleWall)), typeof(PushMoverOut).GetConstructor(new Type[] {typeof(IMovingCollidable), typeof(Vector2)})}
 
             };
---
+++ Sprint/Levels/GapTile.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using Sprint.Interfaces;
+
+namespace Sprint.Levels
+{
+    internal class GapTile : ITile, ICollidable
+    {
+        ISprite sprite;
+        Vector2 position;
+        Rectangle bounds;
+
+        public GapTile(ISprite sprite, Vector2 position, Vector2 size)
+        {
+            this.sprite = sprite;
+            this.position = position;
+            bounds = new Rectangle((int)position.X, (int)position.Y, (int)size.X, (int)size.Y);
+        }
+
+        public void Draw(SpriteBatch spriteBatch, GameTime gameTime)
+        {
+            sprite.Draw(spriteBatch, position, gameTime);
+        }
+
+        public Rectangle GetBoundingBox()
+        {
+            return bounds;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            sprite.Update(gameTime);
+        }
+    }
+}
