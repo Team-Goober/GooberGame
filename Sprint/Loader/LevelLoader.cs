e3bd4e28dd8c5320be14b9e8d83f61d0dd991851
Bill Yang <yang.6152@buckeyemail.osu.edu>
2024-03-08 20:31:44 -0500

    Checkpoint 5

--- Sprint/Loader/LevelLoader.cs
+++ Sprint/Loader/LevelLoader.cs
@@ -50,7 +50,7 @@ namespace Sprint.Loader
                 objectManager.AddRoom(BuildRoomManager(data, i));
             }
 
-            objectManager.SwitchRoom(13);
+            objectManager.SwitchRoom(16);
 
         }
 
