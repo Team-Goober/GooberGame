4364c627aaa2f5f67fdcb29e80a57a096a51f14e
Pqvqn <pavanarauch@hotmail.com>
2024-03-08 23:27:44 -0500

    Middle click selects next room better

--- Sprint/Loader/LevelLoader.cs
+++ Sprint/Loader/LevelLoader.cs
@@ -25,6 +25,9 @@ namespace Sprint.Loader
         private ItemFactory itemFactory;
         private EnemyFactory enemyFactory;
 
+        private Rectangle[] doorBounds;
+        private IDoor[,] doorsPerSide;
+
         public LevelLoader(ContentManager newContent, GameObjectManager objectManager, SpriteLoader spriteLoader, IInputMap inputTable)
         {
             this.content = newContent;
@@ -49,9 +52,30 @@ namespace Sprint.Loader
 
             objectManager.ClearRooms();
 
+
+            // make commands for moving between rooms
+            doorBounds = new Rectangle[] { new Rectangle((int)data.TopDoorPos.X, (int)data.TopDoorPos.Y, (int)data.DoorSize.X, (int)data.DoorSize.Y),
+                new Rectangle((int)data.BottomDoorPos.X, (int)data.BottomDoorPos.Y, (int)data.DoorSize.X, (int)data.DoorSize.Y),
+                new Rectangle((int)data.LeftDoorPos.X, (int)data.LeftDoorPos.Y, (int)data.DoorSize.X, (int)data.DoorSize.Y),
+                new Rectangle((int)data.RightDoorPos.X, (int)data.RightDoorPos.Y, (int)data.DoorSize.X, (int)data.DoorSize.Y)};
+
+            doorsPerSide = new IDoor[4, data.Rooms.Count];
+
             // Load all rooms by index using RoomLoader
             for (int i = 0; i < data.Rooms.Count; i++) {
                 objectManager.AddRoom(BuildRoomManager(data, i));
+            }
+
+            // Make a command that checks all doors at its position for switching rooms when middle clicked
+            for (int i=0; i<4; i++)
+            {
+                IDoor[] slice = new IDoor[data.Rooms.Count];
+                for (int j=0; j < slice.Length; j++)
+                {
+                    slice[j] = doorsPerSide[i, j];
+                }
+                inputTable.RegisterMapping(new ClickInBoundsTrigger(ClickInBoundsTrigger.MouseButton.Middle, doorBounds[i]),
+                    new SwitchRoomFromDoorsCommand(slice, objectManager));
             }
 
             // TODO: replace this with loaded value from file
@@ -81,10 +105,17 @@ namespace Sprint.Loader
             }
 
             // spawn player on other side of room
-            rom.Add(MakeDoor(lvl, rd.TopExit, lvl.TopDoorPos, lvl.BottomSpawnPos));
-            rom.Add(MakeDoor(lvl, rd.BottomExit, lvl.BottomDoorPos, lvl.TopSpawnPos));
-            rom.Add(MakeDoor(lvl, rd.LeftExit, lvl.LeftDoorPos, lvl.RightSpawnPos));
-            rom.Add(MakeDoor(lvl, rd.RightExit, lvl.RightDoorPos, lvl.LeftSpawnPos));
+            IDoor[] doors = { MakeDoor(lvl, rd.TopExit, lvl.TopDoorPos, lvl.BottomSpawnPos),
+                MakeDoor(lvl, rd.BottomExit, lvl.BottomDoorPos, lvl.TopSpawnPos),
+                MakeDoor(lvl, rd.LeftExit, lvl.LeftDoorPos, lvl.RightSpawnPos),
+                MakeDoor(lvl, rd.RightExit, lvl.RightDoorPos, lvl.LeftSpawnPos) };
+
+            for(int i=0; i<doors.Length; i++)
+            {
+                rom.Add(doors[i]);
+                // make commands if clicked
+                doorsPerSide[i, roomIndex] = doors[i];
+            }
 
             //Load Floor tile 
             float x = lvl.FloorGridPos.X; float y = lvl.FloorGridPos.Y;
@@ -125,10 +156,6 @@ namespace Sprint.Loader
             DoorReference doorRef = lvl.DoorReferences[exit.Door];
             //Parameter list is too long
             IDoor door = doorFactory.MakeDoor(doorRef.Type, lvl.SpriteFile, doorRef.SpriteName, position, lvl.DoorSize, lvl.OpenDoorSize, spawnPosition, exit.AdjacentRoom);
-
-            //TODO: find a better place for this
-            inputTable.RegisterMapping(new ClickCollidableTrigger(ClickCollidableTrigger.MouseButton.Middle, door, objectManager),
-                new SwitchRoomCommand(door, Vector2.Zero));
             return door;
         }
 
