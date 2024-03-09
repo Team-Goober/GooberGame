686564af079858e41d4c0dd4973664ac68bcaeb2
Pqvqn <pavanarauch@hotmail.com>
2024-03-08 22:40:13 -0500

    ClickCollidableTrigger, for clicking inside an object

---
+++ Sprint/Input/ClickCollidableTrigger.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Sprint.Interfaces;
+using Sprint.Levels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sprint.Input
+{
+    internal class ClickCollidableTrigger : IInputTrigger
+    {
+        private MouseButton button; // Which button to check for
+        private bool heldPreviously; // Whether last cycle was pressed
+        private bool triggered; // Whether all requirements have been satisfied this cycle
+
+        private ICollidable target;
+        private GameObjectManager objmanager;
+
+        public enum MouseButton
+        {
+            Left,
+            Right,
+            Middle
+        }
+
+        public ClickCollidableTrigger(MouseButton button, ICollidable collidable, GameObjectManager objManager)
+        {
+            this.button = button;
+            this.heldPreviously = false;
+            this.triggered = false;
+            this.objmanager = objManager;
+            this.target = collidable;
+        }
+
+        public bool IsSatisfied()
+        {
+            return triggered;
+        }
+
+        public void UpdateInput(GameTime gameTime, KeyboardState keys, MouseState mouse)
+        {
+            bool pressed = false;
+
+            switch (button)
+            {
+                case MouseButton.Left:
+                    pressed = mouse.LeftButton == ButtonState.Pressed;
+                    break;
+                case MouseButton.Right:
+                    pressed = mouse.RightButton == ButtonState.Pressed;
+                    break;
+                case MouseButton.Middle:
+                    pressed = mouse.MiddleButton == ButtonState.Pressed;
+                    break;
+            }
+
+            // Trigger only when press is begun
+            if (pressed && !heldPreviously)
+            {
+                triggered = target.GetBoundingBox().Contains(mouse.X, mouse.Y);
+            }
+            else
+            {
+                triggered = false;
+            }
+
+            heldPreviously = pressed;
+
+        }
+    }
+}
