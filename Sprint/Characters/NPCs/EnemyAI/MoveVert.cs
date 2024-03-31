9da166f6c127d1d57a9b6b91c6a224d021a70489
MinsooKim142 <khjun7830@gmail.com>
2024-03-31 16:42:58 -0400

    created MoveAI class

---
+++ Sprint/Characters/NPCs/EnemyAI/MoveVert.cs
@@ -0,0 +1,69 @@
+using Microsoft.Xna.Framework.Graphics;
+using Sprint.Interfaces;
+using Microsoft.Xna.Framework;
+using Sprint.Commands.SecondaryItem;
+using System;
+using Sprint.Projectile;
+using Sprint.Sprite;
+using Sprint.Levels;
+
+namespace Sprint.Characters
+{
+    public class MoveVert
+    {
+        private float elapsedTime;
+        private Timer timeAttack;
+        private Vector2 moveDirection; // Movement direction for the random pattern
+        private ICommand projectileCommand;
+        private SimpleProjectileFactory itemFactory;
+        private Vector2 initialPosition;
+        private string lastAnimationName;
+        Physics physics;
+
+        public MoveVert(Physics physics)
+           
+        {
+
+            this.physics = physics;
+
+            // Initialize the move direction randomly
+            RandomizeMoveDirection();
+        }
+
+
+
+
+        // Move AI for MoveVert
+        private void MoveAI(GameTime gameTime)
+        {
+            float speed = 50; // Adjust the speed as needed
+            float moveTime = 2; // Time before changing direction (in seconds)
+
+            if (elapsedTime > moveTime)
+            {
+                // Change direction after the specified time
+                RandomizeMoveDirection();
+                elapsedTime = 0;
+            }
+
+            // Move in the current direction
+            Vector2 newPosition = physics.Position + moveDirection * speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            physics.SetPosition(newPosition);
+        }
+
+
+
+        // Generate a random movement direction for DogEnemy
+        private void RandomizeMoveDirection()
+        {
+            // Generate a random movement direction
+            Random random = new Random();
+            float angle = (float)random.NextDouble() * MathHelper.TwoPi;
+            moveDirection = new Vector2((float)Math.Cos(angle), (float)Math.Sin(angle));
+
+            // Normalize the moveDirection vector
+            moveDirection.Normalize();
+        }
+
+    }
+}
