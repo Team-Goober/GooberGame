691cdd4123f2c7557df4e93bb86b19944532a291
MinsooKim142 <khjun7830@gmail.com>
2024-04-01 18:03:51 -0400

    Got rid of comments

--- Sprint/Functions/Collision/PushMoverBlock.cs
+++ Sprint/Functions/Collision/PushMoverBlock.cs
@@ -16,34 +16,7 @@ namespace Sprint.Functions.Collision
 
             // overlap is directed into the static collider; we want to move outwards
             this.receiver = (IMovingCollidable)receiver;
-
-            //if (receiver is MoveWallTile tile && effector is Player link)
-            //{
-
-
-
-            //    //get center points of the rectangle for tile
-            //    int tileCenterX = tile.GetBoundingBox().X + (tile.GetBoundingBox().Right - tile.GetBoundingBox().X) / 2;
-            //    int tileCenterY = tile.GetBoundingBox().Y + (tile.GetBoundingBox().Bottom - tile.GetBoundingBox().Y) / 2;
-
-
-            //    Vector2 wallTile = new Vector2(tileCenterX, tileCenterY);
-
-            //    //gets center points for rectangle of link
-            //    int linkCenterX = link.BoundingBox.X + (link.BoundingBox.Right - tile.BoundingBox.X) / 2;
-            //    int linkCenterY = link.BoundingBox.Y + (link.BoundingBox.Right - tile.BoundingBox.Y) / 2;
-
-            //    Vector2 linkTile = new Vector2(linkCenterX, linkCenterY);
-
-            //    Vector2 calcDiff = wallTile -linkTile;
-
-            //    distance = calcDiff * overlap;
-
-
-            //}
-            
-
-
+           
             distance = -overlap * 5;
             
         }
