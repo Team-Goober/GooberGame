cc534380a48565ab0b3d6b06ad796da4405e0ddd
Sam E <espanioly.1@osu.edu>
2024-04-03 18:00:16 -0400

    updated stuff n sheets look cooler now!!!

--- Sprint/Characters/Player.cs
+++ Sprint/Characters/Player.cs
@@ -17,6 +17,9 @@ namespace Sprint.Characters
         public Inventory inventory;
 
         private ISprite sprite;
+        private ISprite defaultSprite;
+        private SpriteLoader spriteLoader;
+        private ISprite damagedSprite;
 
         private Physics physics;
 
@@ -59,17 +62,19 @@ namespace Sprint.Characters
 
             //Initialize physics and objectManager
             physics = new Physics(Vector2.Zero);
+            this.spriteLoader = spriteLoader;
 
             inventory = new Inventory();
 
             //Loads sprite for link
-            sprite = spriteLoader.BuildSprite("playerAnims", "player");
+            sprite = spriteLoader.BuildSprite("playerAnims" , "player");
+            damagedSprite = spriteLoader.BuildSprite("playerDamagedAnims" , "player");
 
             // Duration of one sword swing or item use
             attackTimer = new Timer(0.5);
             castTimer = new Timer(0.5);
             // Duration of the damage state
-            damageTimer = new Timer(0.3);
+            damageTimer = new Timer(0.1);
 
             objectManager = null;
 
@@ -121,30 +126,31 @@ namespace Sprint.Characters
             {
                 case Directions.RIGHT:
                     sprite.SetAnimation("swordRight");
+                    damagedSprite.SetAnimation("swordRight");
                     swordRec = new Rectangle((int)physics.Position.X, (int)physics.Position.Y - swordWidth / 2, swordLength, swordWidth);
                     break;
                 case Directions.LEFT:
                     sprite.SetAnimation("swordLeft");
+                    damagedSprite.SetAnimation("swordLeft");
                     swordRec = new Rectangle((int)physics.Position.X - swordLength, (int)physics.Position.Y - swordWidth / 2, swordLength, swordWidth);
                     break;
                 case Directions.UP:
                     sprite.SetAnimation("swordUp");
+                    damagedSprite.SetAnimation("swordUp");
                     swordRec = new Rectangle((int)physics.Position.X - swordWidth / 2, (int)physics.Position.Y - swordLength, swordWidth, swordLength);
                     break;
                 case Directions.DOWN:
                     sprite.SetAnimation("swordDown");
+                    damagedSprite.SetAnimation("swordDown");
                     swordRec = new Rectangle((int)physics.Position.X - swordWidth / 2, (int)physics.Position.Y, swordWidth, swordLength);
                     break;
                 default:
                     break;
             }
 
-            
             swordCollision = new SwordCollision(swordRec, this);
             
             objectManager.Add(swordCollision);
-            
-            
         }
 
         //Cast according to direction
@@ -167,15 +173,19 @@ namespace Sprint.Characters
             {
                 case Directions.RIGHT:
                     sprite.SetAnimation("castRight");
+                    damagedSprite.SetAnimation("castRight");
                     break;
                 case Directions.LEFT:
                     sprite.SetAnimation("castLeft");
+                    damagedSprite.SetAnimation("castLeft");
                     break;
                 case Directions.UP:
                     sprite.SetAnimation("castUp");
+                    damagedSprite.SetAnimation("castUp");
                     break;
                 case Directions.DOWN:
                     sprite.SetAnimation("castDown");
+                    damagedSprite.SetAnimation("castDown");
                     break;
                 default:
                     break;
@@ -199,18 +209,22 @@ namespace Sprint.Characters
                 if (Facing == Directions.DOWN)
                 {
                     sprite.SetAnimation("downStill");
+                    damagedSprite.SetAnimation("downStill");
                 }
                 else if (Facing == Directions.LEFT)
                 {
                     sprite.SetAnimation("leftStill");
+                    damagedSprite.SetAnimation("leftStill");
                 }
                 else if (Facing == Directions.UP)
                 {
                     sprite.SetAnimation("upStill");
+                    damagedSprite.SetAnimation("upStill");
                 }
                 else if (Facing == Directions.RIGHT)
                 {
                     sprite.SetAnimation("rightStill");
+                    damagedSprite.SetAnimation("rightStill");
                 }
             }
             else if (baseAnim == AnimationCycle.Walk)
@@ -218,18 +232,22 @@ namespace Sprint.Characters
                 if (Facing == Directions.DOWN)
                 {
                     sprite.SetAnimation("down");
+                    damagedSprite.SetAnimation("down");
                 }
                 else if (Facing == Directions.LEFT)
                 {
                     sprite.SetAnimation("left");
+                    damagedSprite.SetAnimation("left");
                 }
                 else if (Facing == Directions.UP)
                 {
                     sprite.SetAnimation("up");
+                    damagedSprite.SetAnimation("up");
                 }
                 else if (Facing == Directions.RIGHT)
                 {
                     sprite.SetAnimation("right");
+                    damagedSprite.SetAnimation("right");
                 }
             }
 
@@ -269,7 +287,6 @@ namespace Sprint.Characters
         {
             // Sets velocity towards down
             physics.SetVelocity(new Vector2(0, speed));
-
             sprite.SetAnimation("down");
             Facing = Directions.DOWN;
             baseAnim = AnimationCycle.Walk;
@@ -282,10 +299,10 @@ namespace Sprint.Characters
 
         public override void TakeDamage()
         {
-
-            sprite.SetAnimation("damage");
+            // ATTENTION
+            defaultSprite = sprite;
+            sprite = damagedSprite;
             damageTimer.Start();
-
         }
 
 
@@ -312,10 +329,10 @@ namespace Sprint.Characters
             damageTimer.Update(gameTime);
             if (damageTimer.JustEnded)
             {
+                Debug.WriteLine("AWWWOOOGAA!!!");
+                sprite = spriteLoader.BuildSprite("playerAnims", "player");
                 returnToBaseAnim();
             }
-
-
             physics.Update(gameTime);
             sprite.Update(gameTime);
         }
