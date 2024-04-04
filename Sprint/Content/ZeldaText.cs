38c96c3bb0ad63fdec58918efc8fe02534168746
y-wani <88449363+y-wani@users.noreply.github.com>
2024-04-03 22:36:15 -0400

    Added Zelda Font

---
+++ Sprint/Content/ZeldaText.cs
@@ -0,0 +1,39 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+namespace Sprint
+{
+    public class ZeldaText
+    {
+        private SpriteFont font;
+        private string text;
+        private Vector2 position;
+        private Color color;
+
+        // Constructor with font name parameter
+        public ZeldaText(string fontName, string text, Vector2 position, Color color, ContentManager content)
+        {
+            // Load the font
+            font = content.Load<SpriteFont>(fontName);
+
+            this.text = text;
+            this.position = position;
+            this.color = color;
+        }
+
+        
+        // Draw method
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.DrawString(font, text, position, color);
+        }
+
+        /*
+         * Example usage:
+            zeldaText = new ZeldaText("Font", "HELLO, ZELDA!", new Vector2(gameWidth / 2, gameHeight / 2), Color.White, Content);
+         */
+
+    }
+}
+
