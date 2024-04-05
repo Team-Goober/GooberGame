09d6bf81f7483f64f2a84226f6db15a83abaf6b7
MinsooKim142 <khjun7830@gmail.com>
2024-04-05 18:56:53 -0400

    Refactored SlimeEnemy

--- Sprint/Characters/NPCs/SlimeEnemy.cs
+++ Sprint/Characters/NPCs/SlimeEnemy.cs
@@ -21,6 +21,7 @@ namespace Sprint.Characters
         private Vector2 moveDirection; // Movement direction for the random pattern
         private SimpleProjectileFactory itemFactory;
         private ICommand projectileCommand;
+        private MoveSlime moveSlime;
 
         private Timer timeAttack;
 
@@ -37,11 +38,9 @@ namespace Sprint.Characters
 
             itemFactory = new SimpleProjectileFactory(spriteLoader, 30, true, objectManager);
 
-            projectileCommand = new ShootArrowCommand(itemFactory);
 
 
-            // Initialize the move direction randomly
-            RandomizeMoveDirection();
+            moveSlime = new MoveSlime(physics);
         }
 
         // Update logic
@@ -53,8 +52,9 @@ namespace Sprint.Characters
             // Calculate movement based on elapsed time for the random pattern
             elapsedTime += (float)gameTime.ElapsedGameTime.TotalSeconds;
 
-            // Move randomly within a specified area
-            MoveRandomly(gameTime);
+
+
+            moveSlime.MoveAI(gameTime);
 
             // Update the sprite and physics
             sprite.Update(gameTime);
@@ -63,36 +63,5 @@ namespace Sprint.Characters
 
         }
 
-        // Move Slime randomly within the game area
-        private void MoveRandomly(GameTime gameTime)
-        {
-            float speed = 50; // Adjust the speed as needed
-            float moveTime = 2; // Time before changing direction (in seconds)
-
-            if (elapsedTime > moveTime)
-            {
-                // Change direction after the specified time
-                RandomizeMoveDirection();
-                elapsedTime = 0;
-            }
-
-            // Move in the current direction
-            Vector2 newPosition = physics.Position + moveDirection * speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
-
-
-            physics.SetPosition(newPosition);
-        }
-
-
-
-        // Generate a random movement direction
-        private void RandomizeMoveDirection()
-        {
-            // Generate a random movement direction
-            Random random = new Random();
-            float angle = (float)random.NextDouble() * MathHelper.TwoPi;
-            moveDirection = new Vector2((float)Math.Cos(angle), (float)Math.Sin(angle));
-
-        }
     }
 }
