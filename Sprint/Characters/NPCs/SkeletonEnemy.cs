fe0c5d470a0d3933c909880b86beea2002fd3aac
MinsooKim142 <khjun7830@gmail.com>
2024-04-05 18:31:18 -0400

    got rid of comments for skeleton.cs

--- Sprint/Characters/NPCs/SkeletonEnemy.cs
+++ Sprint/Characters/NPCs/SkeletonEnemy.cs
@@ -79,54 +79,5 @@ namespace Sprint.Characters
 
         }
 
-        //// Move Skeleton randomly within the game area
-        //private void MoveRandomly(GameTime gameTime)
-        //{
-        //    float speed = 50; // Adjust the speed as needed
-        //    float moveTime = 2; // Time before changing direction (in seconds)
-
-        //    if (elapsedTime > moveTime)
-        //    {
-        //        // Change direction after the specified time
-        //        RandomizeMoveDirection();
-        //        elapsedTime = 0;
-        //    }
-
-        //    // Move in the current direction
-        //    Vector2 newPosition = physics.Position + moveDirection * speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
-
-        //    CheckBounds(newPosition, 3);
-
-        //    physics.SetPosition(newPosition);
-        //}
-
-        ////ensures that the enemy always stays within windows of the game
-        //private void CheckBounds(Vector2 pos, float scale)
-        //{
-        //    //int gameX = Goober.gameWidth;
-        //    //int gameY = Goober.gameHeight;
-
-        //    //makes the enemy go to the other direction when it reaches a certain distance so that it doesnt go over window
-        //    //if (pos.X + scale > gameX)
-        //    //{
-        //    //    moveDirection.X = -moveDirection.X;
-
-        //    //}
-
-        //    //if (pos.Y + scale > gameY)
-        //    //{
-        //    //    moveDirection.Y = -moveDirection.Y;
-        //    //}
-        //}
-
-        //// Generate a random movement direction
-        //private void RandomizeMoveDirection()
-        //{
-        //    // Generate a random movement direction
-        //    Random random = new Random();
-        //    float angle = (float)random.NextDouble() * MathHelper.TwoPi;
-        //    moveDirection = new Vector2((float)Math.Cos(angle), (float)Math.Sin(angle));
-
-        //}
     }
 }
