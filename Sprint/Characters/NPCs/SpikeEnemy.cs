e642ad3d746a75fe2b8891a5d971eeb6f0257223
MinsooKim142 <khjun7830@gmail.com>
2024-04-05 22:56:01 -0400

    Deleted comments

--- Sprint/Characters/NPCs/SpikeEnemy.cs
+++ Sprint/Characters/NPCs/SpikeEnemy.cs
@@ -63,36 +63,5 @@ namespace Sprint.Characters
 
         }
 
-        //// Move Spike randomly within the game area
-        //private void MoveRandomly(GameTime gameTime)
-        //{
-        //    float speed = 50; // Adjust the speed as needed
-        //    float moveTime = 2; // Time before changing direction (in seconds)
-
-        //    if (elapsedTime > moveTime)
-        //    {
-        //        // Change direction after the specified time
-        //        RandomizeMoveDirection();
-        //        elapsedTime = 0;
-        //    }
-
-        //    // Move in the current direction
-        //    Vector2 newPosition = physics.Position + moveDirection * speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
-
-
-        //    physics.SetPosition(newPosition);
-        //}
-
-
-
-        //// Generate a random movement direction
-        //private void RandomizeMoveDirection()
-        //{
-        //    // Generate a random movement direction
-        //    Random random = new Random();
-        //    float angle = (float)random.NextDouble() * MathHelper.TwoPi;
-        //    moveDirection = new Vector2((float)Math.Cos(angle), (float)Math.Sin(angle));
-
-        //}
     }
 }
