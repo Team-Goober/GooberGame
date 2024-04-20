807b9f1e4aa48ed452285db62f45d703d6e8f535
Pqvqn <pavanarauch@hotmail.com>
2024-04-20 01:36:51 -0400

    modifications

--- Sprint/Items/Effects/SpawnLavaEffect.cs
+++ Sprint/Items/Effects/SpawnLavaEffect.cs
@@ -9,30 +9,18 @@ namespace Sprint.Items.Effects
     internal class SpawnLavaEffect : IEffect
     {
 
-        private Queue<LavaBucket> movingHooks = new(); // List of hooks that have not yet pierced something
-
         public void Execute(Player player)
         {
             // Create new projectile
             LavaBucket lava = (LavaBucket)player.GetProjectileFactory().CreateFromString("lavabucket", player);
             lava.Create();
-            movingHooks.Enqueue(lava);
         }
 
         public void Reverse(Player player)
         {
-            // Retract existing projectiles
-            LavaBucket h;
-            while (movingHooks.TryDequeue(out h))
-            {
-                h.Retract();
-            }
+            // Don't do anything
         }
 
-        public Queue<LavaBucket> GetHooks()
-        {
-            return movingHooks;
-        }
 
     }
 }
